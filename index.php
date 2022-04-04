b92431967d47b96476c29536626a702255dff20e
josemojena <jose31309@gmail.com>
2022-04-04 16:01:18 +0200

    Remove symfony event-dispatcher component

--- index.php
+++ index.php
@@ -7,14 +7,10 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 $containerBuilder = new ContainerBuilder();
-//$containerBuilder->register('event_dispatcher', EventDispatcher::class);
-
 $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/config'));
 
 try {
     $loader->load('services.yaml');
-
-    //$dispatcher = $containerBuilder->get('event_dispatcher')->addListener('order.confirmed', array());
     $application = $containerBuilder->get('application');
     $application->bootstrap();
 
