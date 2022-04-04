3b5c88a8e1cecd6511428081a074257e059d12d0
josemojena <jose31309@gmail.com>
2022-04-04 17:50:01 +0200

    Remove unused function

--- src/Infrastructure/UI/Console/Util/ConsoleHelper.php
+++ src/Infrastructure/UI/Console/Util/ConsoleHelper.php
@@ -12,15 +12,4 @@ class ConsoleHelper
         print(join("", array_fill(0, 20, "*")));
         print("\n");
     }
-
-    public static function printLine($message, $number = null)
-    {
-        if ($number == null) {
-            printf("%s", $message);
-        } else {
-            printf("%d- %s", $number, $message);
-        }
-
-
-    }
 }
